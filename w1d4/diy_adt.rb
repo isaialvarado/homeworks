21db91c421b004a2f2766257fd9948c3cc26a5db
Joel Alvarado <isaialvarado@Joels-MacBook-Pro.local>
2016-11-17 23:25:18 -0800

    Create Stack and Queue classes

---
+++ w1d4/diy_adt.rb
@@ -0,0 +1,35 @@
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack.push(el)
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    stack
+  end
+end
+
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.push(el)
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    queue
+  end
+end
