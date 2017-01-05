2203bedc8163222eebd28c22839191a1f9cdb348
Joel Alvarado <isaialvarado@users.noreply.github.com>
2017-01-04 16:32:12 -0800

    Update giphy li component

--- w7d3/giphy/components/giphys_index_items.jsx
+++ w7d3/giphy/components/giphys_index_items.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const GiphysIndexItems = ({giphy}) => (
   <figure>
-    <img src={giphy.images.fixed_height.url} alt="giphy image"></img>
+    <img src={giphy.images.fixed_height.url} />
   </figure>
 );
 
